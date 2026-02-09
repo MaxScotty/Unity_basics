c03764af84874cad0b41dc6fdf6a35c684fd2f43
Max Scott <maxscoottgames@gmail.com>
2026-02-09 17:22:42 +0300

    another base

--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -4,51 +4,84 @@ using UnityEngine;
 
 public class Test : MonoBehaviour
 {
-    //health
-    [SerializeField] int _playerHealth = 100; //changing inside Unity
-    float _playerSpeed = 0.5f;
-    bool _isGrounded = true;
-    string _playerName = "Hero";
-    Vector2 _playerPos = new Vector2(-5f, 0f);
+    [SerializeField]int _playerHealth = 100;
+    int _playerShield = 50;
 
-    Rigidbody2D _rb;
+    public int PlayerHealth
+    {
+        get {return _playerHealth;}
+        set {_playerHealth = value;}
+    }
+
+    public int PlayerShield
+    {
+        get { return _playerShield ; }
+        set { _playerShield = value; }
+    }
 
     private void Start()
     {
-        //gameObject.transform.position = _playerPos;
-        _rb = GetComponent<Rigidbody2D>();
-        _rb.gravityScale = 0;
-
+        Debug.Log("You took " + DamageTaken(80) + " in damage");
     }
 
-    private void Update()
+    int DamageTaken(int damage)
     {
-        
-    }
+        int damageTaken;
 
-    //moving horizontally
-    /*
-    Rigidbody2D _rb;
-
-    float _walkSpeed;
-    float _inputHorizontal;
-
-    private void Start()
-    {
-        _rb = gameObject.GetComponent<Rigidbody2D>();
-
-        _walkSpeed = 0.5f;
-
-    }
-
-    private void Update()
-    {
-        _inputHorizontal = Input.GetAxisRaw("Horizontal");
-
-        if (_inputHorizontal != 0)
+        if (damage < PlayerShield && damage == PlayerShield)
         {
-            _rb.AddForce(new Vector2(_inputHorizontal * _walkSpeed, 0f));
+            Debug.Log("Shield is not destroyed!");
+            damageTaken = 0;
         }
+
+        return damageTaken;
     }
-    */
 }
+
+
+//moving horizontally
+/*
+float _playerSpeed = 0.5f;
+bool _isGrounded = true;
+string _playerName = "Hero";
+Vector2 _playerPos = new Vector2(-5f, 0f);
+
+Rigidbody2D _rb;
+
+float _walkSpeed;
+float _inputHorizontal;
+
+private void Start()
+{
+    _rb = gameObject.GetComponent<Rigidbody2D>();
+
+    _walkSpeed = 0.5f;
+
+}
+
+private void Update()
+{
+    _inputHorizontal = Input.GetAxisRaw("Horizontal");
+
+    if (_inputHorizontal != 0)
+    {
+        _rb.AddForce(new Vector2(_inputHorizontal * _walkSpeed, 0f));
+    }
+}
+
+if (damage < PlayerShield)
+        {
+            Debug.Log("Shield isn't destroyed!");
+            damageTaken = 0;
+        }
+        else if (damage == PlayerShield)
+        {
+            Debug.Log("Shield destroyed!");
+            damageTaken = 0;
+        }
+        else
+        {
+            Debug.Log("Shield destroyed and damage taken!");
+            damageTaken = damage - PlayerShield;
+        }
+*/
